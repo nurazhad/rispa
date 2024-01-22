3457bc6143840b8428bafcdbaef90525b218b065
Nendy Azhad Nurahman <azhadnurahmann@outlook.com>
2024-01-22 20:59:41 +0700

    change bg

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -22,7 +22,7 @@ const Base = ({
   return (
     <>
     
-    <div className="bg-[url('/images/posts/triya.png')] bg-contain">
+    <div className="bg-[url('/images/posts/bg1.jpg')] bg-cover">
       <Head>
         {/* title */}
         <title>
